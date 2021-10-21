e6fec7d9501c8bf48c162841dd1c4ae0c1e9a58e
Marco Tesoro <80710549+MarcoT96@users.noreply.github.com>
2021-10-22 00:13:46 +0200

    Add files via upload

---
+++ Exercise 07/07.1/gas/clean.sh
@@ -0,0 +1,7 @@
+rm -rf *gofr_*
+rm -rf *gave_*
+rm -rf *epot_*
+rm -rf *pres_*
+rm -rf frames/*.xyz
+rm -rf seed.out
+rm -rf config.final
---
+++ Exercise 07/07.1/random.h
@@ -0,0 +1,49 @@
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
+
+#ifndef __Random__
+#define __Random__
+
+class Random {
+
+private:
+  int m1,m2,m3,m4,l1,l2,l3,l4,n1,n2,n3,n4;
+
+protected:
+
+public:
+  // constructors
+  Random();
+  // destructor
+  ~Random();
+  // methods
+  void SetRandom(int * , int, int);
+  void SaveSeed();
+  double Rannyu(void);  //RV Uniformly Distributed in [0,1)
+  double Rannyu(double min, double max);  //RV Uniformly Distributed in [min,max)	
+  double Gauss(double mean, double sigma);  //Box-Muller
+  double Lorentzian(double gamma, double mean);
+  double Exp(double lambda);
+  double Exp(double lambda, double min, double max);
+  double Theta(void);  //p(Theta)=1/2 sin(Theta)
+  		       //Solid Angle
+};
+
+#endif // __Random__
+
+/****************************************************************
+*****************************************************************
+    _/    _/  _/_/_/  _/       Numerical Simulation Laboratory
+   _/_/  _/ _/       _/       Physics Department
+  _/  _/_/    _/    _/       Universita' degli Studi di Milano
+ _/    _/       _/ _/       Prof. D.E. Galli
+_/    _/  _/_/_/  _/_/_/_/ email: Davide.Galli@unimi.it
+*****************************************************************
+*****************************************************************/
